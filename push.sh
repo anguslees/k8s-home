5774c42f212ef9103b6a008b079054f31c162e5c
Angus Lees <gus@inodes.org>
2018-04-07 18:41:03 +1000

    New default for KUBECONFIG

--- push.sh
+++ push.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# scp root@kube.lan:/etc/kubernetes/admin.conf /tmp/kubeconfig
-
-KUBECONFIG=/tmp/kubeconfig
+: ${KUBECONFIG:=$HOME/.kube/homeconf}
 KUBECFG_JPATH=$HOME/go/src/github.com/ksonnet/kubecfg/lib:$HOME/src/ksonnet-lib:$HOME/src/gus-sre-kube-manifests/lib
 export KUBECONFIG KUBECFG_JPATH
 
